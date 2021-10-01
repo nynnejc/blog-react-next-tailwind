e8f14f99c84dbab8598ef177eeb045a6cf664126
Nynne Christoffersen <dkNynChr@A-CSLYL63.corp.lego.com>
2021-10-01 16:22:29 +0200

    layout, styling

--- pages/_app.js
+++ pages/_app.js
@@ -3,21 +3,27 @@ import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto my-10 w-8/12">
+      <header>
+        <h1 className="text-6xl font-bold text-center hover:text-blue-200">
+          Kære Computer Blog
+        </h1>
+        <nav className="my-4">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/">
+                <a className="text-purple-400 hover:text-green-900">Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -10,19 +10,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="my-4">
         <h1>Kaere Computer Blog</h1>
       </main>
 
       <div>
         {blogPosts.map((item) => (
           <div key={item.slug}>
-            <div>
+            <div className="my-4">
               <Link href={`/blog/${item.slug}`}>
                 <a>{item.title}</a>
               </Link>
             </div>
-            <div>{item.date}</div>
+            <div className="my-4">{item.date}</div>
             <div>{item.content}</div>
           </div>
         ))}
