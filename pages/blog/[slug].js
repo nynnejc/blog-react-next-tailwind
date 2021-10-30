7b5554a59bf8e8c74303c16635ac6c8447bb2012
Nynne Christoffersen <nynne.christoffersen@lego.com>
2021-10-30 16:27:19 +0200

    parse md files with grat-matter

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,8 +1,13 @@
 import Head from "next/head";
 import { format, parseISO } from "date-fns";
-import { blogPosts, getAllPosts } from "../../lib/data";
+import renderToString from "next-mdx-remote/render-to-string";
+import hydrate from "next-mdx-remote/hydrate";
+
+import { getAllPosts } from "../../lib/data";
 
 export default function BlogPage({ title, date, content }) {
+  const hydratedContent = hydrate(content);
+
   return (
     <div>
       <Head>
@@ -11,32 +16,38 @@ export default function BlogPage({ title, date, content }) {
       </Head>
 
       <main>
-        <div className="">
-          <h2 className="text-2xl font-bold">{title}</h2>
-          <div className="my-4 text-gray-600 text-md">
+        <div className="border-b-2 border-gray-200 mb-4">
+          <h2 className="text-3xl font-bold">{title}</h2>
+          <div className="text-gray-600 text-md">
             {format(parseISO(date), "MMMM do, uuu")}
           </div>
-          <div>{content}</div>
         </div>
+        <div className="prose">{hydratedContent}</div>
       </main>
     </div>
   );
 }
 
 export async function getStaticProps(context) {
-  console.log(context);
   const { params } = context;
+  const allPosts = getAllPosts();
+  const { data, content } = allPosts.find((item) => item.slug === params.slug);
+  const mdxSource = await renderToString(content);
+
   return {
-    props: blogPosts.find((item) => item.slug === params.slug),
+    props: {
+      ...data,
+      date: data.date.toISOString(),
+      content: mdxSource,
+    },
   };
 }
 
 export async function getStaticPaths() {
-  getAllPosts();
   return {
-    paths: blogPosts.map((item) => ({
+    paths: getAllPosts().map((post) => ({
       params: {
-        slug: item.slug,
+        slug: post.slug,
       },
     })),
     fallback: false,
